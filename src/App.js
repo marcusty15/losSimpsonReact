408b9a0093156e159e8cc41243df25522cab7e53
marcusty15 <marco.troconis15@gmail.com>
2022-06-22 18:32:59 -0400

    se corrigio el path de inicio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path="/losSimpsonReact" element={<Home />}/>
         <Route path='/blog' element={<Blog />}/>
         <Route path='/contacto' element={<Contact />}/>
       </Routes>
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link to="/" className="nav-link active" aria-current="page">Inicio</Link>
+                                <Link to="/losSimpsonReact" className="nav-link active" aria-current="page">Inicio</Link>
                             </li>
                             <li className="nav-item">
                                 <Link to="/blog" className="nav-link" >Blog</Link>
